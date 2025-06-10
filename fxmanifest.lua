a20396db92edd27c82d93da6a9ab5dde1d154667
Eazy <ttv.zoods@gmail.com>
2025-06-09 23:44:27 -0400

    Added All Files Via Upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+game 'gta5'
+fx_version 'cerulean'
+
+author 'damneazy'
+description 'A simple ox_lib based pvp menu | discord.gg/eazymods'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
+
+server_script 'server.lua'
+client_script 'client.lua'
+
+lua54 'yes'
